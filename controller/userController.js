70e1a01746cd1c2ac5d83e0bb1c01f1b0125d5be
Levi Gatimu <getmorelev@gmail.com>
2026-03-12 08:21:24 +0200

    Updates: 2026-03-12T06:21:21.997Z

--- controller/userController.js
+++ controller/userController.js
@@ -21,29 +21,38 @@ export  const getUserById = async (req, res) => {
     }
 }
 
-export const createUser = async (req , res) =>{
+export const createUser = async (res, res) => {
     try{
-
+        const user = new Users(req.body);
+        await user.save()
+        res.json(user);
+    }catch(err){
+        conole.log(err);
+        process.exit(1);
     }
 }
 
 export const updateUser = async (req, res) => {
     try{
-        const user = await User.findByIdAndUpdate(
+        const user = await Users.findByIdAndUpdate(
             req.params.id,
             req.body,
-            {  new : true , runvalidators: true}
-        );
-        if(!user) return res.status(404).json({err: "User not found"});
-        res.json(user);
+            { new : true , runvalidators: true});
+        if(!user) return res.status(404).json({err: "User was not found."});
+        res.status(200).json(user)
     }catch(err){
         console.log(err);
         process.exit(1);
     }
 }
 
-export deleteUser = async (req, res) => {
+export const deleteUser = async (req, res) => {
     try{
-
+        const user = await Users.findByIdAndDelete(req.params.id);
+        if(!user) return res.status(404).json({err: "User was not found."});
+        res.status(200).json({message : "User deleted succesfully"});
+    } catch(err){
+        console.log(err);
+        process.exit(1);
     }
 }
